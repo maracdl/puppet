30a5149b24690c464e82d15aa7b48bccd60d0bb5
Mara Dias <maracdl@yahoo.com.br>
2020-03-31 12:21:52 +0100

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,20 +1,24 @@
 class minecraft { 
-  file {'/apt/minecraft':
+  file {'/opt/minecraft':
           ensure => directory,
    }
-   file {'/apt/minecraft/minecraft_server.jar':
+   file {'/opt/minecraft/minecraft_server.jar':
           ensure => file'
           source => '',
    }
    package {'java':
             ensure => present,
    }
-   file {'/apt/minecraft/eula.txt':
+   file {'/opt/minecraft/eula.txt':
           ensure => file,
           content => 'eula=true',
    }
    file {'/etc/systemd/system/minecraft.service':
           ensure => file,
           source => 'puppet::///modules/minecraft/minecraft.service'
+   }
+   service {'minecraft':
+          ensure => running,
+          enable => true,
    }
 }
